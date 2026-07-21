7764855acffd11b2f65417239afb6bad2609a835
rkorona <korona.starx@gmail.com>
2026-07-21 17:34:09 +0800

    up

--- composite-builds/build-logic/common/src/main/java/com/itsaky/androidide/build/config/BuildConfig.kt
+++ composite-builds/build-logic/common/src/main/java/com/itsaky/androidide/build/config/BuildConfig.kt
@@ -36,7 +36,7 @@ object BuildConfig {
   /** The target SDK version. */
   const val targetSdk = 37
 
-  const val ndkVersion = "27.3.13750724"
+  const val ndkVersion = "29.0.14206865"
 
   /** The source and target Java compatibility. */
   val javaVersion = JavaVersion.VERSION_21
